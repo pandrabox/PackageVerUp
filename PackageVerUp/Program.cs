0a004f813174d5ba0272c03031b753d2934efc19
pandravrc <162582491+pandravrc@users.noreply.github.com>
2025-02-03 07:20:59 +0900

    initial

---
+++ PackageVerUp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class PackageVerUp
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("使用方法: PackageVerUp <ファイルパス>");
+            return;
+        }
+
+        string filePath = args[0];
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("指定されたファイルが見つかりません。");
+            return;
+        }
+
+        try
+        {
+            string content = File.ReadAllText(filePath);
+            string pattern = "\"version\": \"(\\d+)\\.(\\d+)\\.(\\d+)\"";
+
+            var match = Regex.Match(content, pattern);
+            if (!match.Success)
+            {
+                Console.WriteLine("バージョン情報が見つかりませんでした。");
+                return;
+            }
+
+            int major = int.Parse(match.Groups[1].Value);
+            int minor = int.Parse(match.Groups[2].Value);
+            int patch = int.Parse(match.Groups[3].Value) + 1;
+
+            string newVersion = $"\"version\": \"{major}.{minor}.{patch}\"";
+            string updatedContent = Regex.Replace(content, pattern, newVersion);
+
+            File.WriteAllText(filePath, updatedContent);
+            Console.WriteLine($"バージョンを {major}.{minor}.{patch} に更新しました。");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"エラーが発生しました: {ex.Message}");
+        }
+    }
+}
---
+++ PackageVerUp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("PackageVerUp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PackageVerUp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("e0670193-d219-4e8b-8758-d368e8e94dd6")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
